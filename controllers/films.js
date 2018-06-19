f73b220ce3ca5dba9f67e7a3957ac3ac74af2ddf
Paul Kelly <paul.kelly20@gmail.com>
2018-06-19 09:24:55 +0100

    Changed post route

--- controllers/films.js
+++ controllers/films.js
@@ -6,14 +6,6 @@ const filmRouter = new express.Router();
 
 const films = Films()
 
-filmRouter.post('/:id/review', function(req, res){
-  const review = new Review(req.body)
-  const index = req.params.id;
-  film = films[index]
-  film.addReview(review);
-  res.json({films : films});
-});
-
 filmRouter.get("/:id", function(req, res){
   const index = req.params.id;
   res.json({film: films[index]})
@@ -35,13 +27,19 @@ filmRouter.get('/', function(req, res){
   res.json({films: films})
 });
 
+filmRouter.post('/:id', function(req, res){
+  const review = new Review(req.body)
+  const index = req.params.id;
+  film = films[index]
+  film.addReview(review);
+  res.json({films : films});
+});
+
 filmRouter.post('/', function(req, res){
   const film = new Film(req.body)
   films.push(film);
   res.json({films : films});
 });
-
-
 
 
 module.exports = filmRouter;
